5d9b0a3be401ca117694876bd56046ead31459ea
kenean dukamo <kad144@zips.uakorn.edu>
2018-04-16 15:44:19 -0400

    Initial Commit

---
+++ Kad144_project3/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Kad144_project3
+//
+//  Created by kenean dukamo on 4/16/18.
+//  Copyright © 2018 kenean dukamo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Kad144_project3/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Kad144_project3
+//
+//  Created by kenean dukamo on 4/16/18.
+//  Copyright © 2018 kenean dukamo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
