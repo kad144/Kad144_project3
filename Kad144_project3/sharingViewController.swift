a0d00873c0edb6130911c7ca6f119b8467faf4b9
kenean dukamo <kad144@zips.uakorn.edu>
2018-05-03 19:54:06 -0400

    added Watch extension to the project

--- Kad144_project3/sharingViewController.swift
+++ Kad144_project3/sharingViewController.swift
@@ -12,10 +12,8 @@ class sharingViewController: UIViewController {
 
     @IBAction func sharingtap()
     {
-        let invitingText = """
-click the link to get our app https://www.google.com/search?safe=active&source=hp&ei=9zTqWvz9CNKJggeZiJeICQ&q=daya+bensa+app&oq=daya+bensa+app&gs_l=psy-ab.3...752.4191.0.4443.15.9.0.0.0.0.1072.1072.7-1.1.0....0...1c.1.64.psy-ab..14.0.0.0...0.0U0Tqwm9IDI
-"""
-        let contolloer = UIActivityViewController(activityItems: [invitingText], applicationActivities: nil)
+        
+        let contolloer = UIActivityViewController(activityItems: ["www.dayebensacoffee.org"], applicationActivities: nil)
         present(contolloer, animated: true, completion: nil)
     }
 
---
+++ dayewatch Extension/InterfaceController.swift
@@ -0,0 +1,50 @@
+//
+//  InterfaceController.swift
+//  dayewatch Extension
+//
+//  Created by kenean dukamo on 5/3/18.
+//  Copyright © 2018 kenean dukamo. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+ 
+    @IBOutlet var orderTimer: WKInterfaceTimer!
+    func wkTimerReset(timer:WKInterfaceTimer,interval:TimeInterval){
+        timer.stop()
+        let time  = NSDate(timeIntervalSinceNow: interval)
+        timer.setDate(time as Date)
+        timer.start()
+    }
+    
+    func timingOrder(){
+
+        wkTimerReset(timer: orderTimer,interval: 0.0)
+        wkTimerReset(timer: orderTimer,interval:300.0) // counts down
+        
+        }
+
+    override func animate(withDuration duration: TimeInterval, animations: @escaping () -> Void) {
+ 
+        
+    }
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        timingOrder()
+        
+    }
+    
+    override func willActivate() {
+       
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+       
+        super.didDeactivate()
+    }
+
+}
